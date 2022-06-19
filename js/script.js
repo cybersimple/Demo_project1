de8c1d076e6a94a1289d72800a70894e41f81a05
cy63rs1mple <cy63rs1mple@gmail.com>
2022-06-19 20:55:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// navbAR
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+console.log(toggleButton);
+const navbarLinks = document.getElementsByClassName('nav_link')[0];
+console.log(navbarLinks);
+
+
+toggleButton.addEventListener('click',()=>{
+  navbarLinks.classList.toggle('active');
+})
+
+// Carousel
+
+const gap = 8;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
